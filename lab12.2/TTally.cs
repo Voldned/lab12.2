052d1cf70c7e879ed9c6857fb8afec3eea5feeae
Владимир Недужко <desktop3o8rpmo@gmail.com>
2023-10-19 12:22:39 +0700

    ''''''''''

---
+++ lab12.2/TTally.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12._2
+{
+    internal class TTallyList
+    {
+        private string[] List = new string[0];
+        
+        public static void Fill(string[] list) 
+        {
+
+        }
+    }
+}
