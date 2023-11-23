beb7bb259cd29e77705513ebbbf07c886db0afef
Владимир Недужко <desktop3o8rpmo@gmail.com>
2023-11-23 14:08:32 +0700

    gotovo

--- lab12.2/Program.cs
+++ lab12.2/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using lab12._2;
+
+TTallyList list = new TTallyList();
+/*list.Fill(Console.ReadLine());
+list.Print();
+list.Delete(Console.ReadLine());
+list.Print();*/
+string cmd = "a";
+while (cmd != "")
+{
+    Console.WriteLine("Введите 1/2/3 для добавления/удаления/просмотра");
+    Console.WriteLine("ДЛЯ ЗАВЕРШЕНИЯ ВВОДА НАЖМИТЕ ENTER");
+    cmd = Console.ReadLine();
+    Console.WriteLine();
+    switch (cmd.Trim())
+    {
+        case "1":
+            Console.WriteLine("Введите название добавляемого товара: ");
+            list.Fill(Console.ReadLine());
+            Console.Clear();
+            break;       
+        case "2":          
+            Console.WriteLine("Введите название удаляемого товара: ");
+            list.Delete(Console.ReadLine());
+            Console.Clear();
+            break;
+        case "3": 
+            list.Print();
+            break;
+        case "":
+            break;
+        default:
+            Console.WriteLine("Несуществующая команда!");
+            break;
+    }
+}
--- lab12.2/TTally.cs
+++ lab12.2/TTally.cs
@@ -8,11 +8,32 @@ namespace lab12._2
 {
     internal class TTallyList
     {
-        private string[] List = new string[0];
-        
-        public static void Fill(string[] list) 
+        protected List<string> tallylist;
+        internal void Fill(string tally) 
         {
-
+            //.WriteLine("Для завершения ввода нажмите Enter...");
+            while (tally != "") 
+            {
+                if (tallylist.Contains(tally.ToLower()) == false)
+                    tallylist.Add(tally.ToLower());
+                tally = Console.ReadLine();
+            }
+        }
+        internal void Delete(string? tally)
+        {
+            //Console.WriteLine("Для завершения удаления нажмите Enter...");
+            while (tally != "")
+            {
+                tallylist.Remove(tally);
+                tally = Console.ReadLine();
+            }
+        }
+        internal void Print()
+        {
+            foreach (string tally in tallylist)
+            { Console.WriteLine(tally); }
+            Console.WriteLine();
         }
     }
 }
+    
